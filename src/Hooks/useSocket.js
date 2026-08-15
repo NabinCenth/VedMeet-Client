ecf7e77eecee603cad5f27858d9f99af19448bfa
Nabin Pokharel <pokhrelnabin17@gmail.com>
2026-08-15 15:24:00 +0545

    Update socket connection to use environment variable for URL

--- src/Hooks/useSocket.js
+++ src/Hooks/useSocket.js
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import io from "socket.io-client";
 const useSocket = () => {
   const [socket, setSocket] = useState(null);
-
+const socketUrl = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
   useEffect(() => {
-    const NewSocket = io("http://localhost:5000");
+    const NewSocket = io(socketUrl);
     setSocket(NewSocket);
 
     NewSocket.on("connect", () => {
